bf06a3766eb7c4ad71149acde521d982df1f5116
Михаил Варин <buhsoft@mail.ru>
2020-12-10 23:44:36 +0500

    Update stupsettings.php

--- stupserver/stupsettings.php
+++ stupserver/stupsettings.php
@@ -5,4 +5,4 @@ $origin='*';					// разрешение браузеру. можно вмест
 define ('ROOTDIR', explode($root_folder, __DIR__)[0].$root_folder.'/');
 
 $uplDir=ROOTDIR.'stupuploads/';	// если ROOTDIR вычислился правильно, здесь будет полный путь к папке загрузки
-$showFilesLimit=10;				// максимальное количество занруженных файлов в папке
+$showFilesLimit=10;				// максимальное количество загруженных файлов в папке
